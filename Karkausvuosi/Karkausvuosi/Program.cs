e87de8e89600dde41478552caa8ee126f17389dd
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-13 13:52:36 +0200

    On kyllä taas ruma itseääntoistava mursu.

---
+++ Karkausvuosi/Karkausvuosi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkausvuosi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna vuosiluku");
+            int Vuosi = int.Parse(Console.ReadLine());
+            if (Vuosi % 4 == 0)
+            {
+                if (Vuosi % 100 == 0)
+                {
+                    if (Vuosi % 400 == 0)
+                    {
+                        Console.WriteLine("Vuosi on karkausvuosi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vuosi ei ole karkausvuosi");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Vuosi on karkausvuosi");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vuosi ei ole karkausvuosi");
+            }
+        }
+    }
+}
